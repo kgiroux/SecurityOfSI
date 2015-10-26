07a0f16c5b3ad2e70c006e2da65066528557dda9
Kévin Giroux <giroux.kevin.fr@gmail.com>
2015-10-26 14:01:44 +0100

    Exercice 2

--- src/bigint/BigIntegerExercise.java
+++ src/bigint/BigIntegerExercise.java
@@ -6,17 +6,22 @@ import java.security.SecureRandom;
 public class BigIntegerExercise {
 
 	public BigIntegerExercise() {
+		//BigInteger a = new BigInteger("79341182948930145");
+	
 	}
 
 	void expression() {
 
 		System.out.println("\nExercise 2:");
 		System.out.println("===========\n");
-
 		/************************************************************
 		 * Insert the code of Exercise 2 below this comment!
 		 ************************************************************/
 
+		BigInteger a = new BigInteger("512"),b = new BigInteger("102"),c = new BigInteger("3"),d = new BigInteger("761"),result;
+		result = ((a.multiply(b)).subtract(c)).divide(c).add((d.multiply(d)).subtract((a.subtract(b))));
+		
+		
 	}
 
 	void comparison() {
